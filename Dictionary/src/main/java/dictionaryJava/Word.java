8cda21a84938be33489025522afc7db3b87ebddd
Dongws6204 <22026532@vnu.edu.vn>
2023-10-06 11:11:52 +0700

    up_2Class

---
+++ Dictionary/src/main/java/dictionaryJava/Word.java
@@ -0,0 +1,55 @@
+package dictionaryJava;
+
+public class Word {
+    private final String wordTarget;
+    private String wordExplain;
+
+    /**
+     * contructornull.
+     *
+     * @null
+     */
+    public Word(String wordTarget) {
+
+        this.wordTarget = wordTarget;
+    }
+
+    /**
+     * contructor.
+     *
+     * @wordTarget English
+     * @wordExplain Vietnamese
+     */
+    public Word(String wordTarget, String wordExplain) {
+        this.wordTarget = wordTarget;
+        this.wordExplain = wordExplain;
+    }
+
+    /**
+     * get english word.
+     *
+     * @return english_word
+     */
+    public String getWordTarget() {
+        return wordTarget;
+    }
+
+    /**
+     * get wordExplain.
+     *
+     * @return Vietnamese
+     */
+    public String getWordExplain() {
+        return wordExplain;
+    }
+
+    /**
+     * set Vietnamese definition.
+     *
+     * @set wordVietnamese
+     */
+    public void setWordExplain(String wordExplain) {
+        this.wordExplain = wordExplain;
+    }
+
+}
