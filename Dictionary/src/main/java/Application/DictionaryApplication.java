ab3a71a4bb0f2dd9a6236a6be038087b5c9fc5ab
Linhchi162 <linhchi1622k4@gmail.com>
2023-10-30 03:48:36 +0700

    add background, change font

--- Dictionary/src/main/java/Application/DictionaryApplication.java
+++ Dictionary/src/main/java/Application/DictionaryApplication.java
@@ -29,7 +29,7 @@ public class DictionaryApplication extends Application {
         primaryStage.show();
 
         primaryStage.setResizable(false);
-        
+
     }
 
     public static void main(String[] args) {
