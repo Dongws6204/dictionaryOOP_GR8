99c43d5a3c5f535367a2d775fede350a1ecfc801
cva2019 <caovananhnd2019@gmail.com>
2023-11-12 02:31:14 +0700

    jdbc

---
+++ Dictionary/src/main/java/JDBC/JDBCconnect.java
@@ -0,0 +1,30 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class JDBCconnect {
+
+        public static void main(String[] args) {
+            Connection connection = null;
+            String jdbcURL = "jdbc:mysql://localhost:3306/dictionarydb";
+            String username = "root";
+            String password = "PHW#84#jeor";
+
+            try {
+                // Tạo kết nối tới cơ sở dữ liệu
+                connection = DriverManager.getConnection(jdbcURL, username, password);
+                System.out.println("Kết nối thành công đến cơ sở dữ liệu MySQL.");
+            } catch (SQLException e) {
+                System.out.println("Lỗi kết nối đến cơ sở dữ liệu MySQL: " + e.getMessage());
+            } finally {
+                try {
+                    if (connection != null) {
+                        connection.close();
+                    }
+                } catch (SQLException e) {
+                    System.out.println("Lỗi khi đóng kết nối: " + e.getMessage());
+                }
+            }
+        }
+}
