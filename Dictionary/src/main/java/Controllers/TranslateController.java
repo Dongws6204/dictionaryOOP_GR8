10305c526a087b1239af4d2d59390a22c17a155d
cva2019 <124920750+cva2019@users.noreply.github.com>
2023-11-28 17:42:59 +0700

    Update TranslateController.java

--- Dictionary/src/main/java/Controllers/TranslateController.java
+++ Dictionary/src/main/java/Controllers/TranslateController.java
@@ -1,63 +1,41 @@
 package Controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
 import dictionaryJava.DictionaryManagement;
+import java.util.List;
 
 public class TranslateController {
-    @FXML
-    private Label EnglishLabel;
-
-    @FXML
-    private ImageView SwitchButton;
-
-    @FXML
-    private Label VietnameseLabel;
-
     @FXML
     private TextArea sourceTextArea;
 
     @FXML
-    private Button TranslateButton;
-
-    @FXML
     private TextArea transalationTextArea;
 
-    private DictionaryManagement dictionaryManager = new DictionaryManagement();
-    private boolean isEnglish = false;
+    private DictionaryManagement dictionaryManagement;
+
+    public TranslateController() {
+        dictionaryManagement = new DictionaryManagement();
+    }
 
     @FXML
     private void initialize() {
+        // Thêm lắng nghe sự kiện khi người dùng nhập dữ liệu vào sourceTextArea
         sourceTextArea.textProperty().addListener((observable, oldValue, newValue) -> {
-            String translation;
-            translation = dictionaryManager.translateFromVietnameseToEnglish(newValue);
-            transalationTextArea.setText(translation);
+            // Kiểm tra người dùng đã nhập từ tiếng Anh hay tiếng Việt
+            if (isEnglishText(newValue)) {
+                // Nếu là tiếng Anh, dịch sang tiếng Việt và hiển thị kết quả trong transalationTextArea
+                String translation = dictionaryManagement.translateFromEnglishToVietnamese(newValue);
+                transalationTextArea.setText(translation);
+            } else {
+                String translation = dictionaryManagement.translateFromVietnameseToEnglish(newValue);
+                transalationTextArea.setText(translation);
+            }
         });
     }
 
-    @FXML
-    void Switch(javafx.scene.input.MouseEvent event) {
-        isEnglish = !isEnglish;
 
-        if (isEnglish) {
-            EnglishLabel.setText("TIẾNG ANH");
-            VietnameseLabel.setText("TIẾNG VIỆT");
-        } else {
-            EnglishLabel.setText("TIẾNG VIỆT");
-            VietnameseLabel.setText("TIẾNG ANH");
-        }
-
-        translate(); // Cập nhật lại dịch vụ khi chuyển đổi ngôn ngữ
+    private boolean isEnglishText(String text) {
+        return text.matches("^[a-zA-Z0-9\\s]+$");
     }
-
-    @FXML
-    public void translate() {
-        String textToTranslate = sourceTextArea.getText().trim();
-        String translation = dictionaryManager.translateFromVietnameseToEnglish(textToTranslate);
-        transalationTextArea.setText(translation);
-    }
-
 }
